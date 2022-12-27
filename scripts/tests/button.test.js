3b7ce0abc24c583db61a6c8ae494788e17dd9c93
seanj06 <seanj06@hotmail.com>
2022-12-27 15:24:38 +0000

    Add dom tests

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -6,12 +6,19 @@ const { TestScheduler } = require("jest");
 const buttonClick = require("../button")
 
 beforeEach( () => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs")
+    let filecontents = fs.readFileSync("index.html", "utf-8")
+    document.open();
+    document.write(filecontents); 
+    document.close();
 });
 
 describe("DOM tests", () => {
     test("expects p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    })
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     })
 })

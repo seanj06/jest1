b478cf2e1e57678fb4e0df9821a126e52fd3d550
seanj06 <seanj06@hotmail.com>
2022-12-27 15:02:17 +0000

    Add mocking test

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { TestScheduler } = require("jest");
+const buttonClick = require("../button")
+
+beforeEach( () => {
+    document.body.innerHTML = "<p id='par'></p>";
+});
+
+describe("DOM tests", () => {
+    test("expects p content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    })
+})
